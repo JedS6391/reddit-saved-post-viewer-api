ea923f6553faffa98b25c7e45bd81b84deb08f25
Jed Simson <jed.simson@gmail.com>
2019-11-03 15:11:43 +1300

    Make number of queue workers configurable.

--- worker.py
+++ worker.py
@@ -15,7 +15,7 @@ from rq import Worker, Queue, Connection
 listen = ['service']
 connection = Redis()
 
-MAX_WORKERS = 2
+DEFAULT_NUMBER_OF_WORKERS = 2
 
 def get_logging_level():
     # Default to error
@@ -30,18 +30,29 @@ def get_logging_level():
 
 logging.basicConfig(level=get_logging_level()) 
 
+def get_number_of_workers():
+    worker_count = DEFAULT_NUMBER_OF_WORKERS
+
+    try:
+        worker_count = int(os.environ['NUMBER_OF_WORKERS'])
+    except KeyError:
+        pass
+
+    return worker_count
+
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
+    number_of_workers = get_number_of_workers()
 
     logger.debug('Initialising workers...')
-    logger.debug('  worker count = %s', MAX_WORKERS)
+    logger.debug('  worker count = %s', number_of_workers)
 
     with Connection(connection):
         worker = Worker(list(map(Queue, listen)))
 
         workers = []
 
-        for i in range(MAX_WORKERS):
+        for i in range(number_of_workers):
             processor = multiprocessing.Process(target=worker.work)
 
             workers.append(processor)
